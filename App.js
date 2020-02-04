2160bdbf29d3e95fbb83a6e467924d750ed59229
aaron <xakong@outlook.com>
2020-02-04 13:19:30 -0500

    updates

--- App.js
+++ App.js
@@ -7,8 +7,13 @@ import GoalInput from './components/GoalInput';
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode, setIsAddMode] = useState(false);
+  console.log('RE-RENDERING COMPONENT');
+  console.log(courseGoals);
 
   const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {
+      return;
+    }
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
@@ -17,6 +22,8 @@ export default function App() {
   };
 
   const removeGoalHandler = goalId => {
+    console.log('TO BE DELETED: ' + goalId);
+    console.log(courseGoals);
     setCourseGoals(currentGoals => {
       return currentGoals.filter(goal => goal.id !== goalId);
     });
